4e9e698cf8d9e3d1b410ce2545187e283c529799
K0nja <mariok1992@gmail.com>
2025-02-25 20:37:13 -0500

    fix typo

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>It's a me, Mario!</h1>
         <p className={styles.description}>
-          I'm a full-stack developer with 10 years of experience. I have tons of backend experince as well as a hobby in game development. Reach out if you'd like to learn more!
+          I'm a full-stack developer with 10 years of experience. I have tons of backend experience as well as a hobby in game development. Reach out if you'd like to learn more!
         </p>
         <p></p>
         <p className={styles.description}>You can find my full resume <b><a href="https://drive.google.com/file/d/1oFyuQyALrBNelPq-VV4Kf8pcKh9bi8BB/view?usp=sharing">here</a></b></p>
